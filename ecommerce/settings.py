8effe2d4f1e9aff280f8f3126ee7fcf6313dc689
saptarsi96 <saptarshi.saha0@gmail.com>
2021-05-22 00:18:49 +0530

    hostallow

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('debug', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
