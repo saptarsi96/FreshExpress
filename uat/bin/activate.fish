24fba02d90c25339d91dc3329a44c7528534f829
saptarsi96 <saptarshi.saha0@gmail.com>
2021-05-22 00:34:51 +0530

    bulk push

--- uat/bin/activate.fish
+++ uat/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/saptarsi/django-ecommerce/uat"
+set -gx VIRTUAL_ENV "/home/saptarsi/FreshExpressCustomer/uat"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
