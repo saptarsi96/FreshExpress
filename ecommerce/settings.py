6b9c4e90c0c9b3af1725df00fbfc33de6a0cdb66
saptarsi96 <saptarshi.saha0@gmail.com>
2021-05-22 09:46:19 +0530

    Heroku 4

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -144,12 +144,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+if DEBUG:
+    STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
