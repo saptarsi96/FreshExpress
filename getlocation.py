fa7a3c1321e76168cdb5a71d9b68fee42e77056b
saptarsi96 <saptarshi.saha0@gmail.com>
2021-05-26 01:19:40 +0530

    Auto location fix trial 1

---
+++ getlocation.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+def getLocation():
+    options = Options()
+    options.add_argument("--use-fake-ui-for-media-stream")
+    timeout = 20
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox") # linux only
+    options.add_argument("window-size=150,150")
+    #options.add_argument("--headless")
+    #options.headless = True # also works
+    driver = webdriver.Chrome(executable_path = './chromedriver', chrome_options=options)
+    driver.get("https://mycurrentlocation.net/")
+    wait = WebDriverWait(driver, timeout)
+    time.sleep(3)
+    longitude = driver.find_elements_by_xpath('//*[@id="longitude"]')
+    longitude = [x.text for x in longitude]
+    longitude = str(longitude[0])
+    latitude = driver.find_elements_by_xpath('//*[@id="latitude"]')
+    latitude = [x.text for x in latitude]
+    latitude = str(latitude[0])
+    driver.quit()
+    return (latitude,longitude)
+print(getLocation())
--- recommendation_engine.py
+++ recommendation_engine.py
@@ -3,6 +3,7 @@ import django
 import requests
 from django.db.models import Max,Min
 from orders.models import Recommendations
+from getlocation import *
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
@@ -29,18 +30,18 @@ R = 6373.0
 
 
 def get_dist(lat1,lon1,lat2,lon2):
-    lat1 = math.radians(lat1)
-    lon1 = math.radians(lon1)
-    lat2 = math.radians(lat2)
-    lon2 = math.radians(lon2)
+    lat1 = math.radians(float(lat1))
+    lon1 = math.radians(float(lon1))
+    lat2 = math.radians(float(lat2))
+    lon2 = math.radians(float(lon2))
     dlon = lon2-lon1
     dlat = lat2-lat1
     a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     return R*c
 
-a,b = get_lat_long("Bits Pilani , Hyderabad, Telengana 500078")
-c,d = get_lat_long("BE-21 Newtown Action Area 1B, Kolkata 700156")
+
+
 
 #print(get_dist(a,b,c,d))
 
@@ -50,8 +51,7 @@ def get_valid_shops():
     prime_delivery=False
     range=2
     valid_shopkeepers = {}
-    customer_lat = 17.41710876415962
-    customer_long = 78.44529794540337
+    customer_lat, customer_long = getLocation()
     shopkeeper_dataset = models.Store.objects.all().filter(shop_status='Open')
     for shopkeeper in shopkeeper_dataset.iterator():
         distance = get_dist(customer_lat,customer_long,shopkeeper.lat,shopkeeper.long)
