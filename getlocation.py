c5802eb1d1a76c183fa97f0f7ec35a06834e3cd9
saptarsi96 <saptarshi.saha0@gmail.com>
2021-05-26 01:51:26 +0530

    Trial 5

--- getlocation.py
+++ getlocation.py
@@ -1,21 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-import time
+from selenium.webdriver import ActionChains
+import time,os
 
 def getLocation():
-    options = Options()
-    options.add_argument("--use-fake-ui-for-media-stream")
-    timeout = 20
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox") # linux only
-    options.add_argument("window-size=150,150")
-    #options.add_argument("--headless")
-    #options.headless = True # also works
-    driver = webdriver.Chrome(executable_path = './chromedriver', chrome_options=options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--window-size=1920x1080")
+    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path = './chromedriver', chrome_options=chrome_options)
     driver.get("https://mycurrentlocation.net/")
-    wait = WebDriverWait(driver, timeout)
+    wait = WebDriverWait(driver, 20)
     time.sleep(3)
     longitude = driver.find_elements_by_xpath('//*[@id="longitude"]')
     longitude = [x.text for x in longitude]
