f734b6cfcb34ac3acda6b87ff3e91981a27e60c1
saptarsi96 <saptarshi.saha0@gmail.com>
2021-05-26 22:52:27 +0530

    pep-8 styling

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -1,28 +1,32 @@
-import json,math,os
+from orders import models as orderdb
+from store import models
+import json
+import math
+import os
 import django
 import requests
-from django.db.models import Max,Min
+from django.db.models import Max, Min
 
 import store
 
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
 django.setup()
-from store import models
-from orders import models as orderdb
-#todoAddress conversion to latitude and longitude
+# todoAddress conversion to latitude and longitude
+
 
 def get_lat_long(location):
     parameters = {
-    "key" : "h0rGftGyPqqLqVZE0b0d1nzKnTAxpuMe",
-    "location" : location
+        "key": "h0rGftGyPqqLqVZE0b0d1nzKnTAxpuMe",
+        "location": location
     }
-    response = requests.get("http://www.mapquestapi.com/geocoding/v1/address",params=parameters)
+    response = requests.get(
+        "http://www.mapquestapi.com/geocoding/v1/address",
+        params=parameters)
     data = json.loads(response.text)['results']
     lat = data[0]['locations'][0]['latLng']['lat']
     lng = data[0]['locations'][0]['latLng']['lng']
-    return lat,lng
+    return lat, lng
 
 
 #!Distance Calculation between 2 points
@@ -30,49 +34,62 @@ def get_lat_long(location):
 R = 6373.0
 
 
-def get_dist(lat1,lon1,lat2,lon2):
+def get_dist(lat1, lon1, lat2, lon2):
     lat1 = math.radians(lat1)
     lon1 = math.radians(lon1)
     lat2 = math.radians(lat2)
     lon2 = math.radians(lon2)
-    dlon = lon2-lon1
-    dlat = lat2-lat1
-    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * \
+        math.cos(lat2) * math.sin(dlon / 2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    return R*c
-
-a,b = get_lat_long("Bits Pilani , Hyderabad, Telengana 500078")
-c,d = get_lat_long("BE-21 Newtown Action Area 1B, Kolkata 700156")
-
-#print(get_dist(a,b,c,d))
+    return R * c
 
 
-#Finding valid list of shops according to given range defined.
+a, b = get_lat_long("Bits Pilani , Hyderabad, Telengana 500078")
+c, d = get_lat_long("BE-21 Newtown Action Area 1B, Kolkata 700156")
+
+# print(get_dist(a,b,c,d))
+
+
+# Finding valid list of shops according to given range defined.
 def get_valid_shops():
-    prime_delivery=False
-    range=2
+    prime_delivery = False
+    range = 2
     valid_shopkeepers = {}
     customer_lat = 17.41710876415962
     customer_long = 78.44529794540337
     shopkeeper_dataset = models.Store.objects.all().filter(shop_status='Open')
     for shopkeeper in shopkeeper_dataset.iterator():
-        distance = get_dist(customer_lat,customer_long,shopkeeper.lat,shopkeeper.long)
-        if(distance<=range):
-            valid_shopkeepers[shopkeeper.name] = [distance,shopkeeper.id]
+        distance = get_dist(
+            customer_lat,
+            customer_long,
+            shopkeeper.lat,
+            shopkeeper.long)
+        if(distance <= range):
+            valid_shopkeepers[shopkeeper.name] = [distance, shopkeeper.id]
             print(valid_shopkeepers[shopkeeper.name])
     if not valid_shopkeepers:
-        prime_delivery=True                   # Found No STORE nearby. Fall back to amazon flex or amazon prime delivery
-    #valid_shopkeepers = {k: v for k, v in sorted(valid_shopkeepers.items(), key=lambda item: item[1])}             For Sorted Values
-    return prime_delivery,valid_shopkeepers
+        # Found No STORE nearby. Fall back to amazon flex or amazon prime
+        # delivery
+        prime_delivery = True
+    # valid_shopkeepers = {k: v for k, v in sorted(valid_shopkeepers.items(),
+    # key=lambda item: item[1])}             For Sorted Values
+    return prime_delivery, valid_shopkeepers
+
 
 def ratings_prepocessor():
     rated_shopkeepers = {}
     shopkeeper_dataset = models.Store.objects.all()
     for shopkeeper in shopkeeper_dataset.iterator():
         rated_shopkeepers[shopkeeper.name] = shopkeeper.rating
-    upper_range = models.Store.objects.all().aggregate(Max('rating'))                   # To calculate max value for that column
-    lower_range = models.Store.objects.all().aggregate(Min('rating'))                   # To calculate min value for that column
-    return rated_shopkeepers,upper_range['rating__max'],lower_range['rating__min']
+    upper_range = models.Store.objects.all().aggregate(
+        Max('rating'))                   # To calculate max value for that column
+    lower_range = models.Store.objects.all().aggregate(
+        Min('rating'))                   # To calculate min value for that column
+    return rated_shopkeepers, upper_range['rating__max'], lower_range['rating__min']
+
 
 def number_of_sucessful_orders():
     sucessful_orders = {}
@@ -81,23 +98,28 @@ def number_of_sucessful_orders():
         sucessful_orders[shopkeeper.name] = shopkeeper.total_orders
     upper_range = models.Store.objects.all().aggregate(Max('total_orders'))
     lower_range = models.Store.objects.all().aggregate(Min('total_orders'))
-    return sucessful_orders,upper_range['total_orders__max'],lower_range['total_orders__min']
+    return sucessful_orders, upper_range['total_orders__max'], lower_range['total_orders__min']
 
-def scaling(OldMax,OldMin,NewMax,NewMin,OldValue):
-    OldRange = (OldMax - OldMin)  
-    NewRange = (NewMax - NewMin)  
+
+def scaling(OldMax, OldMin, NewMax, NewMin, OldValue):
+    OldRange = (OldMax - OldMin)
+    NewRange = (NewMax - NewMin)
     NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
     return NewValue
 
+
 def ratingupdater():
-    review_dataset=orderdb.Review.objects.all().order_by('-order_id')[:1]       #Get the most recent element of a order
+    review_dataset = orderdb.Review.objects.all().order_by(
+        '-order_id')[:1]  # Get the most recent element of a order
     for reviews in review_dataset.iterator():
         order_entity = orderdb.Order.objects.get(id=reviews.order_id)
         store_entity_orders_table = order_entity.store_id
 
-        store_entity_stores_table = models.Store.objects.get(id=store_entity_orders_table)
-        prev_val = store_entity_stores_table.rating * store_entity_stores_table.total_orders        #Prev Rating
-        prev_val += reviews.userrating                                                              #Updated Rating                
+        store_entity_stores_table = models.Store.objects.get(
+            id=store_entity_orders_table)
+        prev_val = store_entity_stores_table.rating * \
+            store_entity_stores_table.total_orders  # Prev Rating
+        prev_val += reviews.userrating  # Updated Rating
         new_val = prev_val / (store_entity_stores_table.total_orders + 1)
         print(round(new_val))
         store_entity_stores_table.rating = new_val
@@ -105,33 +127,41 @@ def ratingupdater():
         store_entity_stores_table.save()
         # if reviews.order_id == orderdb.Order.objects.all().filter('order_id'):
         #     print(orderdb.Order.objects.all().filter('store'))
-        #print(reviews.order_id,reviews.userrating)
+        # print(reviews.order_id,reviews.userrating)
 
 
 def recommendation_algo():
     # IF shop is not in range is ineligble move to prime delivery
-    prime,shop_list = get_valid_shops()
-    rated_shopkeepers,upper_shopkeeper,lower_shopkeeper = ratings_prepocessor()
-    sucessful_orders,upper_successful_orders,lower_successful_orders = number_of_sucessful_orders()
-    if(prime==True):
+    prime, shop_list = get_valid_shops()
+    rated_shopkeepers, upper_shopkeeper, lower_shopkeeper = ratings_prepocessor()
+    sucessful_orders, upper_successful_orders, lower_successful_orders = number_of_sucessful_orders()
+    if(prime):
         return "Redirect to amazon warehouse for delivery via amazon flex or prime delivery."
     finalshoplist = {}
-    for i,j in shop_list.items():
-        val = j[0]*0.40*2.5                          #Distance Calculation 40% weightage   (*2.5 to scale since range is defined at 2 KMS)
-        adder1 = scaling(upper_shopkeeper,lower_shopkeeper,5,0,rated_shopkeepers[i])        #Scaled
-        val += adder1 * 0.20                     #Ratings Calculation 20% weightage
-        adder2 = scaling(upper_successful_orders,lower_successful_orders,5,0,sucessful_orders[i])
-        val += adder2*0.40                       #Succesful orders Calculation 40% weightage
-        val=round(val,1)
-        finalshoplist[i]=[val,j[1]]
+    for i, j in shop_list.items():
+        # Distance Calculation 40% weightage   (*2.5 to scale since range is
+        # defined at 2 KMS)
+        val = j[0] * 0.40 * 2.5
+        adder1 = scaling(upper_shopkeeper, lower_shopkeeper,
+                         5, 0, rated_shopkeepers[i])  # Scaled
+        val += adder1 * 0.20  # Ratings Calculation 20% weightage
+        adder2 = scaling(
+            upper_successful_orders,
+            lower_successful_orders,
+            5,
+            0,
+            sucessful_orders[i])
+        val += adder2 * 0.40  # Succesful orders Calculation 40% weightage
+        val = round(val, 1)
+        finalshoplist[i] = [val, j[1]]
         print(finalshoplist[i])
-    finalshoplist = {k: v for k, v in sorted(finalshoplist.items(), key=lambda item: item[1],reverse=True)} 
+    finalshoplist = {
+        k: v for k,
+        v in sorted(
+            finalshoplist.items(),
+            key=lambda item: item[1],
+            reverse=True)}
     return finalshoplist
-   
 
-
-   
-    
-        #query = Recommendations(shop=i,score=j)
-        #query.save()
-
+    #query = Recommendations(shop=i,score=j)
+    # query.save()
