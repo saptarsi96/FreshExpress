97f50821708049b7ea1c18868ee0bdd6a0642caf
Shailesh Navale <snavale122@gmail.com>
2021-05-29 22:53:54 +0530

    Shopadmin Navigation Menu

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
+from os import name
+import shopadmin
 from django.urls import path, include
 from accounts import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register', views.Register.as_view(), name='register'),
-    path('profile', views.profile, name='profile')
+    path('profile', views.profile, name='profile'),
+    path('shopadmin/',include('shopadmin.urls',namespace='shopadmin'))
 ]
