d5f6d0fd3b97ef6f67acef5b84561498dac58b7a
Shailesh Navale <snavale122@gmail.com>
2021-05-30 15:08:47 +0530

    Fix 4

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=1000)
+    pincode = models.CharField(max_length=7)
+    city = models.CharField(max_length=255)
+    lat = models.FloatField(default=17.41710876415962)
+    long = models.FloatField(default=78.44529794540337)
